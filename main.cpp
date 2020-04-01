6d6a60bb39830f1070c74500255d3d3f74718e2b
charley <charleychang@gmail.com>
2020-04-01 23:14:17 +0800

    modified the main() function to adapte to MacOS

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QDebug>
 #include <iostream>
-#include "cserialports.h"
+#include <string>
+// #include "cserialports.h"
 using namespace std;
 int main(void)
 {
+    cout << string(1000,'\n');
+
     cout << "+++This is a demo to test UWB localization System.+++\n"
          << "++++Author: CharleyChang    Data: 2020. 03. 25+++\n";
 
@@ -16,7 +19,8 @@ int main(void)
         cout << "Please input the number of tags[1~8]:";
         cin >> nTagNum;
         if (cin.fail() || nTagNum < 1 || nTagNum > 8){
-            cin.clear(); cin.sync();
+            cin.clear();
+            cin.ignore(1000,'\n');
             cout << "The input is not a number, please input a number between 1 to 8!" << endl;
         }
     }
@@ -27,36 +31,40 @@ int main(void)
         cout << "Please input the number of anchors[1~4]:";
         cin >> nAnchNum;
         if (cin.fail() || nAnchNum < 1 || nAnchNum > 4){
-            cin.clear(); cin.sync();
+            cin.clear(); 
+            cin.ignore(1000,'\n');
             cout << "The input is not a number, please input a number between 1 to 4!" << endl;
         }
     }
     cout << "There are " << nAnchNum << " anchors in the system.\n";
-    double dX = 0.0; double dY = 0.0; double dZ = 0.0;
     for (int nid = 1; nid <= nAnchNum; nid++)
     {
+        double dX = 0.0; double dY = 0.0; double dZ = 0.0;
         cout << "Input the coordination of the anchor No." << nid << "\n";
         cout << "X:"; cin >> dX;
         while (cin.fail()){
-            cin.clear(); cin.sync();
+            cin.clear(); 
+            cin.ignore(1000,'\n');
             cout << "Please input a number!" << endl;
             cout << "X:"; cin >> dX;
         }
         cout << "Y:"; cin >> dY;
         while (cin.fail()){
-            cin.clear(); cin.sync();
+            cin.clear();
+            cin.ignore(1000,'\n');
             cout << "Please input a number!" << endl;
             cout << "Y:"; cin >> dY;
         }
         cout << "Z:"; cin >> dZ;
         while (cin.fail()){
-            cin.clear(); cin.sync();
+            cin.clear();
+            cin.ignore(1000,'\n');
             cout << "Please input a number!" << endl;
             cout << "Z:"; cin >> dZ;
         }
     }
-    CSerialPorts *device = new CSerialPorts;
-    device->testDevices();
+    // CSerialPorts *device = new CSerialPorts;
+    // device->testDevices();
 
     cout << "End of Processing.\n";
     return 0;
